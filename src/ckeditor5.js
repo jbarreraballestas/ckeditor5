bec1b6eb3daa736aed57e34facee39be391b90a7
JBarreraBallestas <jbarreraballestas@correo.unicordoba.edu.co>
2022-07-22 13:43:08 -0500

    Changes date

--- src/ckeditor5.js
+++ src/ckeditor5.js
@@ -5,6 +5,7 @@ import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
 import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline';
 import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough';
+import Code from '@ckeditor/ckeditor5-basic-styles/src/code';
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading';
 import Font from '@ckeditor/ckeditor5-font/src/font';
@@ -29,10 +30,21 @@ import Subscript from '@ckeditor/ckeditor5-basic-styles/src/subscript';
 import Superscript from '@ckeditor/ckeditor5-basic-styles/src/superscript';
 import HorizontalLine from '@ckeditor/ckeditor5-horizontal-line/src/horizontalline';
 // import Markdown from '@ckeditor/ckeditor5-markdown-gfm/src/markdown';
-
-
-
-
+import Image from '@ckeditor/ckeditor5-image/src/image';
+import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
+import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
+import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
+import ImageResize from '@ckeditor/ckeditor5-image/src/imageresize';
+import LinkImage from '@ckeditor/ckeditor5-link/src/linkimage';
+import SimpleUploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/simpleuploadadapter';
+import Base64UploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/base64uploadadapter';
+import ImageInsert from '@ckeditor/ckeditor5-image/src/imageinsert';
+import AutoImage from '@ckeditor/ckeditor5-image/src/autoimage';
+import CodeBlock from '@ckeditor/ckeditor5-code-block/src/codeblock';
+import FindAndReplace from '@ckeditor/ckeditor5-find-and-replace/src/findandreplace';
+import SpecialCharacters from '@ckeditor/ckeditor5-special-characters/src/specialcharacters';
+import SpecialCharactersEssentials from '@ckeditor/ckeditor5-special-characters/src/specialcharactersessentials';
+import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
 
 export default class ClassicEditor extends ClassicEditorBase { }
 ClassicEditor.builtinPlugins = [
@@ -43,6 +55,7 @@ ClassicEditor.builtinPlugins = [
     Italic,
     Underline,
     Strikethrough,
+    Code,
     Font,
     BlockQuote,
     Table, TableToolbar, TableCaption, TableProperties, TableCellProperties, TableColumnResize,
@@ -57,43 +70,29 @@ ClassicEditor.builtinPlugins = [
     Subscript,
     Superscript,
     HorizontalLine,
+    Image, ImageStyle, ImageResize, ImageToolbar, ImageCaption, LinkImage, ImageInsert, AutoImage,
+    SimpleUploadAdapter,
+    Base64UploadAdapter,
+    CodeBlock,
+    FindAndReplace,
+    SpecialCharacters,
+    SpecialCharactersEssentials,
+    MediaEmbed
+
 ];
 
 ClassicEditor.defaultConfig = {
     toolbar: {
-        items: [
-            'removeFormat',
-            '|',
-            'alignment',
-            'bold',
-            'italic',
-            'underline',
-            'strikethrough',
-            'link',
-            '|',
-            'fontSize',
-            'fontFamily',
-            '|',
-            'bulletedList',
-            'numberedList',
-            'todoList',
-            '|',
-            'outdent', 'indent',
-            'blockQuote',
-            'undo',
-            'redo',
-            'insertTable',
-            'subscript', 'superscript',
-            '|',
-            'horizontalLine'
-        ]
+        items:
+            ["removeFormat", "bold", "italic", "alignment", "link", "bulletedList", "numberedList", "todoList", "outdent", "indent", "undo", "redo", "|", "fontBackgroundColor", "fontColor", "fontFamily", "fontSize", "|", "imageUpload", 'mediaEmbed', /* "imageInsert", */ "insertTable", "|", "code", "blockQuote", "codeBlock", "findAndReplace", "horizontalLine", "specialCharacters", "strikethrough", "subscript", "superscript", "underline"],
+        shouldNotGroupWhenFull: true
     },
     table: {
         contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells', 'tableProperties', 'tableCellProperties', 'toggleTableCaption']
     },
     fontSize: {
         options: [
-            '8','10','12','14','default','18','20','22','24','26','28','30',
+            '8', '10', '12', '14', 'default', '18', '20', '22', '24', '26', '28', '30',
         ],
         supportAllValues: true
     },
@@ -103,8 +102,9 @@ ClassicEditor.defaultConfig = {
     //     ],
     //     supportAllValues: true
     // },
+    image: { toolbar: ["imageTextAlternative", "imageStyle:inline", "imageStyle:block", "imageStyle:side"] },
     // This value must be kept in sync with the language defined in webpack.config.js.
-    language: 'en'
+    language: 'en',
 };
 
 window.ClassicEditor = ClassicEditor;
